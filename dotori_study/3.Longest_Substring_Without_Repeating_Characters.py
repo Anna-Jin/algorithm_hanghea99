1680de4d4201981ab41fc05a3786a1cef45796e1
Anna-Jin <anna.jin.6848@gmail.com>
2022-05-23 01:05:18 +0900

    중복 문자가 없는 가장 긴 문자열을 찾아라

--- dotori_study/3.Longest_Substring_Without_Repeating_Characters.py
+++ dotori_study/3.Longest_Substring_Without_Repeating_Characters.py
@@ -3,3 +3,29 @@
 
 # 중복 문자가 없는 가장 긴 문자열을 찾아라
 
+s = "abcabcbb"
+
+
+def longest(s):
+    left = 0
+    word = ''
+    result = 0
+
+    for i in range(len(s)):
+        right = i + 1
+
+        if right >= len(s):
+            break
+
+        word += s[left]
+        left = len(word) - 1
+
+        if s[right] in word:
+            result = len(word) - 1
+            word = ''
+            continue
+
+    return result
+
+
+print(longest(s))
