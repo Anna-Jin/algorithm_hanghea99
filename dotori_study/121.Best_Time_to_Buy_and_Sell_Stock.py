ea694beecb2f3ab690160652cf6876d391606773
Anna-Jin <anna.jin.6848@gmail.com>
2022-05-23 21:37:47 +0900

    leet 121. Best Time to Buy and Sell Stock

--- dotori_study/121.Best_Time_to_Buy_and_Sell_Stock.py
+++ dotori_study/121.Best_Time_to_Buy_and_Sell_Stock.py
@@ -1,3 +1,19 @@
 # leet 121. Best Time to Buy and Sell Stock
 # interview 12. 주식을 사고팔기 가장 좋은 시점
+import sys
 
+prices = [7, 1, 5, 3, 6, 4]
+
+
+def maxProfit(prices):
+    profit = 0
+    min_price = prices[0]
+    for num in prices:
+        if num < min_price:
+            min_price = num
+        elif num - min_price > profit:
+            profit = num - min_price
+    return profit
+
+
+print(maxProfit(prices))
