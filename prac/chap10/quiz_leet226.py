c40186ff83f9f50c182ef790713804f9cfa46038
Anna-Jin <anna.jin.6848@gmail.com>
2022-05-24 19:30:08 +0900

    14장 트리 - 45. 이진 트리 반전 (Invert Binary Tree)

--- prac/chap10/quiz_leet226.py
+++ prac/chap10/quiz_leet226.py
@@ -1,11 +1,35 @@
 # 14장 트리 - 45. 이진 트리 반전 (Invert Binary Tree)
 
 # 중앙을 기준으로 이진 트리를 반전시켜라.
-import collections
+
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+def make_tree_by(list, index):
+    parent = None
+
+    if index < len(list):
+        value = list[index]
+        if value is None:
+            return
+
+        parent = TreeNode(value)
+        parent.left = make_tree_by(list, 2 * index + 1)
+        parent.right = make_tree_by(list, 2 * index + 2)
+
+    return parent
+
 
 
 def invertTree(root):
-    queue = collections.deque([root])
+    queue = deque([root])
 
     while queue:
         node = queue.popleft()
@@ -16,5 +40,9 @@ def invertTree(root):
             queue.append(node.right)
 
     return root
+
+root = [4,2,7,1,3,6,9]
+
+print(invertTree(make_tree_by(root, 0)))
 
 
