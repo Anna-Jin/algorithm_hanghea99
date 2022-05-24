e130f6fca00ec658cacda0246a4532a0d0ee91fc
Anna-Jin <anna.jin.6848@gmail.com>
2022-05-24 17:15:24 +0900

    14장 트리 - 42. 이진 트리의 최대 깊이 (Maximum Depth of Binary Tree)

--- prac/chap9/quiz_leet104.py
+++ prac/chap9/quiz_leet104.py
@@ -1,18 +1,35 @@
 # 14장 트리 - 42. 이진 트리의 최대 깊이 (Maximum Depth of Binary Tree)
 # 이진 트리의 최대 깊이를 구하라
-
 from collections import deque
-from make_tree import make_tree_by
 
-list = [3,9,20,None,None,15,7]
+
+class TreeNode:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+
+def make_tree_by(list, index):
+    parent = None
+
+    if index < len(list):
+        value = list[index]
+        if value is None:
+            return
+
+        parent = TreeNode(value)
+        parent.left = make_tree_by(list, 2 * index + 1)
+        parent.right = make_tree_by(list, 2 * index + 2)
+
+    return parent
+
 
 
 def max_depth(list):
-    root = make_tree_by(list, 0)
-    if not root:
-        return 0
 
-    q = deque([root])
+    q = deque([list])
     depth = 0
 
     while q:
@@ -27,4 +44,6 @@ def max_depth(list):
     return depth
 
 
-print(max_depth(list))
+list = [1,2,3,4,5,6,6,8,9]
+
+print(max_depth(make_tree_by(list, 0)))
